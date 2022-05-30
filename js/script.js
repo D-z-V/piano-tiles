e1a623341a1c63847a8eb717c2e015e938804eda
Dev Bhandari <51533301+D-z-V@users.noreply.github.com>
2022-05-30 21:08:03 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -149,6 +149,8 @@ hacker.addEventListener('click', function (e) {
         }
         if (!started) {
             start.onclick = function (e) {
+                title.style.fontSize = '44px'
+                score.style.fontSize = '27px'
                 start.style.display = 'none'
                 timerElm.style.display = 'block'
                 score.style.display = 'block'
@@ -425,6 +427,8 @@ hackerpp.addEventListener('click', function (e) {
     }
 
     backbtn.addEventListener('click', function (e1) {
+        title.style.fontSize = '50px'
+        score.style.fontSize = '30px'
         normal.style.display = 'block'
         hacker.style.display = 'block'
         hackerpp.style.display = 'block'
